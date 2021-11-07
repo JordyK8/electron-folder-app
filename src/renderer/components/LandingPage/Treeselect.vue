12dc5f9b855953cacbbbdff5986c8057142021ec
Joca Koyi <jocakoyi@MacBook-Pro.home>
2021-11-07 18:50:23 +0100

    starting with ui for python/javascript project to manage and watch home folders and features

---
+++ src/renderer/components/LandingPage/Treeselect.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+  <treeselect
+    :multiple="true"
+    :options="options"
+    placeholder="Select your favourite(s)..."
+    v-model="value"
+    />
+  <treeselect-value :value="value" />
+</div>
+</template>
+
+<script>
+import Treeselect from '@riophae/vue-treeselect';
+
+export default {
+  components: { Treeselect },
+  props: ['options'],
+  created() {
+    console.log(this.items);
+  },
+  data: () => ({
+    value: [],
+  }),
+  watch: {
+    value(val) {
+      if (val) {
+        this.$emit('addFolder', val);
+      }
+    },
+  },
+};
+</script>
+
+<style src="@riophae/vue-treeselect/dist/vue-treeselect.min.css"></style>
